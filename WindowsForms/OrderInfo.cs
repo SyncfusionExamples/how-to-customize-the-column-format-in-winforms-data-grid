3f9d5de0f34a5e1ba2023b7e9ee974ccb4485773
Dhanasekar Mohanraj <dhanasekar.mohanraj@syncfusion.com>
2020-11-30 11:41:56 +0530

    How to customize the column format in WinForms DataGrid (SfDataGrid)?

---
+++ WindowsForms/OrderInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Percentage_Column
+{
+    public class OrderInfo
+    {
+        string text;
+        double values;
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+        public double Value
+        {
+            get { return values; }
+            set { values = value; }
+        }
+        public OrderInfo(string text,double values)
+        {
+            this.Text = text;
+            this.Value = values;
+        }
+    }
+}
---
+++ WindowsForms/OrderInfoCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Percentage_Column
+{
+    public class OrderInfoCollection
+    {
+        private ObservableCollection<OrderInfo> _orders;
+        public ObservableCollection<OrderInfo> Orders
+        {
+            get { return _orders; }
+            set { _orders = value; }
+        }
+        public OrderInfoCollection()
+        {
+            _orders = new ObservableCollection<OrderInfo>();
+            this.GenerateOrders();
+        }
+        private void GenerateOrders()
+        {
+            _orders.Add(new OrderInfo("t1" ,0.5678));
+            _orders.Add(new OrderInfo("t2" ,0.6671));
+            _orders.Add(new OrderInfo("t3" ,0.3675));
+            _orders.Add(new OrderInfo("t4" ,0.3709));           
+        }
+    }
+}
